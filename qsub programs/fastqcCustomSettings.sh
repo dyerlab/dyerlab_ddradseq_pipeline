2bed391e1b588dde7473a10e3f8a93ad8c526150
Tyler-Hohenstein <hohensteinta@mymail.vcu.edu>
2020-11-16 09:50:45 -0500

    Create fastqcCustomSettings.sh

---
+++ qsub programs/fastqcCustomSettings.sh
@@ -0,0 +1,4 @@
+export PATH="/gpfs_fs/hohensteinta/bin/FastQC:$PATH"
+export PATH="/gpfs_fs/hohensteinta/bin/stacks-2.54:$PATH"
+source /usr/global/jdk8/enable_java.sh
+source /opt/rh/devtoolset-7/enable
